67baa04ef1e26d4e32deda281f51ba140544c2a8
ehtiram-ullah <ehtiramullah786@gmail.com>
2024-12-13 19:48:50 +0500

    updated

--- customers/src/app.js
+++ customers/src/app.js
@@ -1,3 +1,17 @@
-const {v4:uuid} = require('uuid');
+const express = require('express')
 
-console.log(uuid());
+const app  = express();
+
+
+app.get('/',(req,res)=>{
+    res.statusCode=200;
+    res.send('the port is '+PORT);
+});
+
+app.post('/',(req,res)=>{
+    res.send('This is a post req, let change something');
+});
+
+const PORT = 3000;
+
+app.listen(PORT,()=>console.log('App listening on port '+PORT));
