12f6a80257f110e560a186363c9221fced059038
Ehtiram-Ullah <ehtiramullah786@gmail.com>
2024-12-13 22:53:13 +0500

    Configure mongodb

--- customers/src/app.js
+++ customers/src/app.js
@@ -1,17 +1,62 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+mongoose.set('strictQuery',false)
 const app  = express();
+//Database password - uc3IK63xbdNiISOs
+app.use(express.json());
 
-
+const customers = [
+    {
+        name:"Jibran",
+        industry:"Football"
+    },
+    {
+        name: "Afsar Ali",
+        industry: "App developer",
+    },
+    {
+        name:"Mazhar Ali",
+        industry:"Music"
+    }
+];
 app.get('/',(req,res)=>{
     res.statusCode=200;
-    res.send('the port is '+PORT);
+    res.send("<h1>Hello Customers</h1>");
+});
+app.get('/api/customers',(req,res)=>{
+    res.statusCode=200;
+    res.send(customers);
+});
+app.post('/api/customers',(req,res)=>{
+    res.statusCode=200;
+    console.log("Hello world");
+    console.log(req.body);
+    customers.push(req.body);
+    
+    res.send("New customer added Successfully!");
 });
 
 app.post('/',(req,res)=>{
     res.send('This is a post req, let change something');
 });
 
-const PORT = 3000;
+const CONNECTION = process.env.CONNECTION;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>console.log('App listening on port '+PORT));
+
+
+const start = async()=>{
+    try{
+
+        await mongoose.connect(CONNECTION);
+        app.listen(PORT,()=>console.log('App listening on port '+PORT));
+    }catch(e){
+        console.log(e.message);
+    }
+    
+}
+
+start();
